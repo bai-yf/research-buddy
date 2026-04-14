e16e661e2e82961dc929c0eb20cf47f307b1f2d1
bai-yf <1605202490@qq.com>
2026-04-14 16:10:31 +0800

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://10.19.126.202:8000";
+const BASE_URL = "https://cool-pianos-care.loca.lt";
 
 export async function askQuestion(sessionId: string, question: string): Promise<string> {
   const res = await fetch(`${BASE_URL}/ask`, {
