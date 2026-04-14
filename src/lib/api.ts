ddda097488ed8ea7a35992ab6f81d820ec57e016
bai-yf <1605202490@qq.com>
2026-04-14 21:38:37 +0800

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://127.0.0.1:8000";
+const BASE_URL = "http://127.0.0.1:8000";
 //const BASE_URL = "https://fragrant-name-unmindful.ngrok-free.dev";
 //const BASE_URL = "https://small-buttons-learn.loca.lt";
 
